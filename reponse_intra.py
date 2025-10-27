98eccd803564324cace278e07fe2d66e410fa120
6266166 <charlienantel@pronatek.ca>
2025-10-27 12:00:54 -0400

    Question 1 (presque temriner, doit faire apparaitre les 10 température) , question 2 pas terminé

--- reponse_intra.py
+++ reponse_intra.py
@@ -1,3 +1,30 @@
 ##########################################
-# Nom, Prénom, <Numéro de DA>
+# Nantel, Charlie, <6266266>
 ##########################################
+import random as rdm
+for i in range(10):
+    temperature = rdm.uniform(20, 35)
+
+def question1():
+
+    if 24 < temperature <=30:
+        message = "ok"
+    elif temperature < 24:
+        message = "Trop froid"
+    elif temperature > 30 :
+        message = "Trop chaud"
+    print("Jour", i, ":", format(temperature, ".1f"), message)
+
+question1()
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+def question2():
+    pop_bacterie_initiale = int(input("Entrez la population de bactérie initiale :"))
+    pop_chaque_heure = pop_bacterie_initiale + np.pi/1.5
+
+
+
+
